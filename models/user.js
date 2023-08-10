ee8c240f093394e8afbc22330b833ccf429b1a80
DanielLM98 <33654417+DanielLM98@users.noreply.github.com>
2023-08-10 22:07:34 +0200

    Añadida funcionalidad recuperar contraseña

--- models/user.js
+++ models/user.js
@@ -16,4 +16,8 @@ module.exports = class User {
         console.log(user)
         return db.execute('INSERT INTO `usuarios` (`ID`, `Nombre`, `Apellido`, `CorreoElectronico`, `Contrasena`, `TipoUsuario`, `Estado`) VALUES (?,?,?,?,?,?,?)', [null, user.nombre, user.apellido, user.email, user.contrasena, user.tipoUsuario, user.estado]);
     }
-};
+
+    static resetPassword(email, contrasena) {
+        return db.execute('UPDATE usuarios SET Contrasena = ? WHERE CorreoElectronico = ?', [contrasena, email]);
+    }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -11,12 +11,12 @@ router.post('/signup', [
     body('nombre').trim().not().isEmpty(),
     body('apellido').trim().not().isEmpty(),
     body('correoElectronico').isEmail().withMessage('Please enter a valid email.')
-        .custom(async (email) => {
-            const user = await User.find(email);
-            if (user[0].length > 0) {
-                return Promise.reject('E-Mail address already exists!');
-            }
-        }).normalizeEmail(),
+    .custom(async(email) => {
+        const user = await User.find(email);
+        if (user[0].length > 0) {
+            return Promise.reject('E-Mail address already exists!');
+        }
+    }).normalizeEmail(),
     body('contrasena').trim().isLength({ min: 8 }),
     body('tipoUsuario').trim().not().isEmpty(),
     body('estado').trim().not().isEmpty()
@@ -28,4 +28,14 @@ router.get('/', (req, res) => {
 
 router.post('/login', authController.login);
 
+router.post('/recovery', [body('correoElectronico').isEmail().withMessage('Please enter a valid email.').custom(
+    async(correoElectronico) => {
+        const user = await User.find(correoElectronico);
+        console.log(user)
+        if (user[0].length == 0) {
+            return Promise.reject('E-Mail address does not exist!');
+        }
+    }).normalizeEmail()], authController.recovery);
+
+router.put('/resetPassword', [body('password').trim().isLength({ min: 8 }).withMessage("La longuitud minima es de 8")], authController.resetPassword);
 module.exports = router;
